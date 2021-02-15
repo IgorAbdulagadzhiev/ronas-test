4db068a45cce38fdc260e210b4dfe794395d2fe4
Igor Abdulagadjiev <s89183906500@gmail.com>
2021-02-15 10:21:40 +0300

    убрал направление ветра

--- src/helpers/createDetailsList.js
+++ src/helpers/createDetailsList.js
@@ -1,6 +1,6 @@
 export default ({ data }) => {
   return [
-    { label: "Ветер", value: `${data?.wind?.speed} м/с, какой` },
+    { label: "Ветер", value: `${data?.wind?.speed} м/с` },
     { label: "Давление", value: `${data?.main?.pressure} рт. ст.` },
     { label: "Влажность", value: `${data?.main?.humidity} рт. ст.` },
     { label: "Облачность", value: `${data?.clouds?.all}%` },
